655b65a213a5d9f3988f7119f2a7579bb4920c54
Rahulsosa0000 <131368502+Rahulsosa0000@users.noreply.github.com>
2025-01-20 18:01:37 +0530

    Update SecurityConfig.java

--- JwtProjectCrudComplate/src/main/java/com/crud/config/SecurityConfig.java
+++ JwtProjectCrudComplate/src/main/java/com/crud/config/SecurityConfig.java
@@ -10,8 +10,9 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -49,17 +50,17 @@ public class SecurityConfig {
 
 	}
 
-//	@Bean
-//	public BCryptPasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-	    daoAuthenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
-		//daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+	    //daoAuthenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
 		daoAuthenticationProvider.setUserDetailsService(userDetailsService);
 		return daoAuthenticationProvider;
 	}
